ccebd1cdad205c2dd3bfd5b7d2be5b788dc06e40
ccuiqqinghhao <33272785+ccuiqqinghhao@users.noreply.github.com>
2018-03-29 18:03:36 +0800

    实现查询所有用户

--- src/main/java/com/example/demo/entity/ResultEntity.java
+++ src/main/java/com/example/demo/entity/ResultEntity.java
@@ -16,22 +16,24 @@ public class ResultEntity<T> {
      */
     private T data;
 
-    public Integer getCode() {
-        return code;
-    }
+
     public void setCode(Integer code) {
         this.code = code;
-    }
-    public String getMsg() {
-        return msg;
     }
     public void setMsg(String msg) {
         this.msg = msg;
     }
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
     public T getData() {
         return data;
     }
-    public void setData(T data) {
-        this.data = data;
+    public String getMsg() {
+        return msg;
     }
 }
